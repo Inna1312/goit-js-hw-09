a5ddcc18ce4b15890ec6998a42bf82d99416d373
Inna1312 <126244200+Inna1312@users.noreply.github.com>
2023-08-09 11:56:44 +0300

    task1-2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,3 +2,28 @@
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
+
+btnStart.addEventListener('click', changeBg);
+btnStop.addEventListener('click', stopChange);
+let idBg;
+
+function changeBg() {
+    idBg = setInterval(() => {
+        body.style.background = getRandomHexColor();
+    }, 1000);
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+};
+
+function stopChange() {
+    clearInterval(idBg);
+    btnStop.disabled = true;
+    btnStart.disabled = false;
+}
+
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
